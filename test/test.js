e6c044f45fefdbc11eab29ddf5da11a8f7ca4636
Ville Lautanala <lautis@gmail.com>
2015-12-28 06:07:48 +0200

    Use ES6 in tests

--- test/test.js
+++ test/test.js
@@ -1,39 +1,39 @@
-var assert = require('assert');
-var rollup = require('rollup');
-var commonjs = require('rollup-plugin-commonjs');
-var coffeePlugin = require('..');
-var coffee = require('coffee-script');
-var fs = require('fs');
+import assert from 'assert';
+import rollup from 'rollup';
+import commonjs from 'rollup-plugin-commonjs';
+import coffeePlugin from '..';
+import coffee from 'coffee-script';
+import fs from 'fs';
 
 process.chdir(__dirname);
 
 describe('rollup-plugin-coffeescript', function() {
   this.timeout(5000);
 
-  it('runs code through coffeescript', function() {
-    var entry = 'sample/basic/main.coffee';
-    var source = fs.readFileSync(entry).toString();
+  it('runs code through coffeescript', () => {
+    const entry = 'sample/basic/main.coffee';
+    const source = fs.readFileSync(entry).toString();
 
     return rollup.rollup({
       entry: entry,
       plugins: [coffeePlugin()]
     }).then(function(bundle) {
-      var generated = bundle.generate();
-      var coffeeOutput = coffee.compile(source, { bare: true });
+      const generated = bundle.generate();
+      const coffeeOutput = coffee.compile(source, { bare: true });
       assert.equal(generated.code.trim(), coffeeOutput.trim());
     });
   });
 
-  it('works with requires when used with commonjs plugin', function() {
-    var entry = 'sample/import-class/main.coffee';
-    var source = fs.readFileSync(entry).toString();
+  it('works with requires when used with commonjs plugin', () => {
+    const entry = 'sample/import-class/main.coffee';
+    const source = fs.readFileSync(entry).toString();
 
     return rollup.rollup({
       entry: entry,
       plugins: [coffeePlugin(), commonjs({ extensions: ['.coffee']})]
     }).then(function(bundle) {
-      var generated = bundle.generate();
-      var code = generated.code;
+      const generated = bundle.generate();
+      const code = generated.code;
       assert.ok(code.indexOf('class') === -1, code);
       assert.ok(code.indexOf('var A = __commonjs(') !== -1, code);
     });
